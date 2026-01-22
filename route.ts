b5e9945ae0b004b32c8a78faf1f353dc0300f68f
sumann <sumannnarya6969@gmail.com>
2026-01-22 20:54:12 +0530

    delete route file

--- route.ts
+++
@@ -1,7 +0,0 @@
-import  {type RouteConfig , index , route} from '@react-router/dev/routes';
-
-export  default [
-      index(file : "routes/home.tsx"),
-      route(path : '/auth' , file : 'routes/auth.tsx'),
-
-] satisfies RouteConfig;
