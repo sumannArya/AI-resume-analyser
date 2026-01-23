12bf5eb71ad9940d547a98b04fbccd10641f5cf5
sumann <sumannnarya6969@gmail.com>
2026-01-23 22:21:35 +0530

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,22 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    {
+      name: "chrome-devtools-fix",
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === "/.well-known/appspecific/com.chrome.devtools.json") {
+            res.setHeader("Content-Type", "application/json");
+            res.end("{}");
+          } else {
+            next();
+          }
+        });
+      },
+    },
+  ],
 });
